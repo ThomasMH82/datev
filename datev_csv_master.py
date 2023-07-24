e1438c2af37ae547e69d8ad8d8e88e7ce2a3f1a3
ThomasMH82 <90532350+ThomasMH82@users.noreply.github.com>
2023-07-24 18:53:32 +0200

    Update datev_csv_master.py

--- datev_csv_master.py
+++ datev_csv_master.py
@@ -130,7 +130,7 @@ if uploaded_file is not None:
 
   # ---- Mainpage ----
   st.title("Datev Board")
-  #st.divider()
+  st.divider()
   #KPI´s
   col1 , col2 = st.columns(2)
    
@@ -140,7 +140,7 @@ if uploaded_file is not None:
   col2.subheader(f"Umsatz 19%:  {monat19gesamt}")
   col2.subheader(f"Steuer 19%: {steuer19}")
   col2.subheader(f"Netto 19%: {netto19}")
-  #st.divider() 
+  st.divider() 
   st.title("Umsatz tageweise")
   col1_1 , col2_1 = st.columns(2)
   col1_1.write("Umsatz 7% tageweise")
