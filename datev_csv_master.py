4cb074e38ebb3b621b8ba128eb1b204156600f8c
ThomasMH82 <90532350+ThomasMH82@users.noreply.github.com>
2023-10-24 11:32:59 +0200

    Update datev_csv_master.py

--- datev_csv_master.py
+++ datev_csv_master.py
@@ -135,12 +135,12 @@ if uploaded_file is not None:
 
   def get_table_download_link(df, filename="data.xlsx", label="Download data as Excel"):
    """Generiert einen Download-Link für ein DataFrame"""
-    towrite = io.BytesIO()
-    df.to_excel(towrite, index=True, engine='openpyxl')  # Schreiben Sie das DataFrame in ein Byte-Objekt
-    towrite.seek(0)  # Gehen Sie zum Beginn des Byte-Objekts zurück
-    b64 = base64.b64encode(towrite.read()).decode()  # Kodieren Sie das Byte-Objekt als base64
-    link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="{filename}">{label}</a>'
-    return link
+      towrite = io.BytesIO()
+      df.to_excel(towrite, index=True, engine='openpyxl')  # Schreiben Sie das DataFrame in ein Byte-Objekt
+      towrite.seek(0)  # Gehen Sie zum Beginn des Byte-Objekts zurück
+      b64 = base64.b64encode(towrite.read()).decode()  # Kodieren Sie das Byte-Objekt als base64
+      link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="{filename}">{label}</a>'
+      return link
 
     # Verwenden Sie Streamlit, um die Pivot-Tabelle auszugeben
   stb_umstz = stb_pivot(df)
